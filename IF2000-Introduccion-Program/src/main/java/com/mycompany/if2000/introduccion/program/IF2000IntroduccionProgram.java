60586591c7a0aa589fd945cc374c3e0876ce9faf
unknown <melaguillen15educ@gmail.com>
2025-08-21 10:43:41 -0600

    prueba rolo

---
+++ IF2000-Introduccion-Program/src/main/java/com/mycompany/if2000/introduccion/program/IF2000IntroduccionProgram.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.if2000.introduccion.program;
+
+/**
+ *
+ * @author Usuario
+ */
+public class IF2000IntroduccionProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("prueba profe");
+    }
+}
